3da3363fb5016468674b7a6635c4d6004a56a38e
Riddhi Barhate <riddhibarhate@gmail.com>
2024-04-13 23:53:44 +0530

    initial commit

---
+++ coding ninjas/Count derangements.cpp
@@ -0,0 +1,21 @@
+#define MOD 1000000007
+#include<vector>
+
+long long int solve(int n){
+            vector<long long int> dp(n+1,0);
+
+      
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = ((i-1)%MOD * ((dp[i-1])%MOD + (dp[i-2])%MOD))%MOD;
+        }
+
+        return dp[n];
+}
+
+long long int countDerangements(int n) {
+    // Write your code here.
+        solve(n);
+}
