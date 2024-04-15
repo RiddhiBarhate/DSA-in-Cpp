1c4d0f9b0bb09530851734ab0186af6591264b66
Riddhi Barhate <riddhibarhate@gmail.com>
2024-04-15 23:50:30 +0530

    Time: 176 ms (90.15%), Space: 215.4 MB (80.11%) - LeetHub

--- 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -1,35 +1,3 @@
-// class Solution {
-// public:
-//     int solve(vector<int>& obstacles, int lane, int pos, vector<vector<int>> &dp){
-//         int n = obstacles.size()-1;
-//         if(pos == n)
-//             return 0;
-
-//         if(dp[lane][pos] != -1){
-//             return dp[lane][pos];
-//         }
-
-//         if(obstacles[pos+1] != lane){
-//             return solve(obstacles, lane, pos+1, dp);
-//         }
-//         else{
-//             int ans = 1e9;
-//             for(int i = 1; i<=3; i++){
-//                 if(obstacles[pos] != i && lane != 1){
-//                     ans = min(ans, 1+solve(obstacles, i, pos, dp));
-//                 }
-//                 dp[lane][pos] = ans;
-//             }
-//         return dp[lane][pos];    
-//         }
-//     }
-
-//     int minSideJumps(vector<int>& obstacles) {
-//         vector<vector<int>> dp(4, vector<int> (obstacles.size(), -1));
-//         return solve(obstacles,2,0,dp);
-//     }
-// };
-// 
 class Solution {
 public:
     int minSideJumps(vector<int>& arr) {
