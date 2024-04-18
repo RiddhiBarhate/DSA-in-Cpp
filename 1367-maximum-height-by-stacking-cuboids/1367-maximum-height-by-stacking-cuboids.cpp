5121c7f2d2d18c8ddf72bf318b24cec6a3f7a25d
Riddhi Barhate <riddhibarhate@gmail.com>
2024-04-18 13:44:04 +0530

    Time: 7 ms (82.81%), Space: 12.1 MB (76.5%) - LeetHub

---
+++ 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(vector<int>& base, vector<int>& newBox){
+        if(newBox[0] <= base[0] && newBox[1] <= base[1] && newBox[2] <= base[2])
+            return true;
+        else
+            return false;
+    }
+
+    int solve(vector<vector<int>>& a){
+        int n = a.size();
+        vector<int> currRow(n+1, 0);
+        vector<int> nextRow(n+1, 0);
+
+        for(int curr=n-1; curr>=0; curr--){
+            for(int prev=curr-1; prev>=-1; prev--){
+                int include=0;
+                if(prev == -1 || check(a[curr], a[prev])){
+                   include = a[curr][2] + nextRow[curr+1];
+                }
+                int exclude = nextRow[prev+1];
+                currRow[prev+1] = max(include,exclude);
+            }
+            nextRow = currRow;
+        }
+        return nextRow[0];
+    }
+
+    int maxHeight(vector<vector<int>>& cuboids) {
+        for(auto &i : cuboids)
+            sort(i.begin(), i.end());
+        sort(cuboids.begin(), cuboids.end());
+        return solve(cuboids);
+    }
+};
