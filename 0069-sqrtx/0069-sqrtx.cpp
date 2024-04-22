d462a2ad93772c8da83b6d8df0f240af014cc1e3
Riddhi Barhate <riddhibarhate@gmail.com>
2024-04-22 23:14:49 +0530

    Time: 0 ms (100%), Space: 7.4 MB (39.18%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long int sqrtroot(int n )
+    {
+        int s=0,e=n;
+        long long int ans=-1;
+        while(s<=e)
+        {
+            long long int mid=s+(e-s)/2;
+            long long square=mid*mid;
+            if(square==n)
+            {
+                return mid;
+            }
+            else if(square<n)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else
+            {
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    int mySqrt(int x) {
+        return sqrtroot(x);
+    }
+};
