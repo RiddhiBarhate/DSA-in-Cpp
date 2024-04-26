ab9f93880d41d92fffdcdc57152b6a4e0ae4bac6
Riddhi Barhate <riddhibarhate@gmail.com>
2024-04-26 15:07:47 +0530

    Time: 32 ms (52.07%), Space: 13.5 MB (19.6%) - LeetHub

--- 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
+++ 1228-minimum-cost-tree-from-leaf-values/1228-minimum-cost-tree-from-leaf-values.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
 
-    int solve(int x, int y, vector<int>& arr,  map<pair<int,int>, int>& maxi, vector<vector<int>>& dp){
+    // int solveMem(int x, int y, vector<int>& arr,  map<pair<int,int>, int>& maxi, vector<vector<int>>& dp){
+    //     int n = arr.size();
+    //     if(x == y)
+    //         return 0;
+
+    //     if(dp[x][y] != -1)
+    //         return dp[x][y];
+
+    //     int ans = INT_MAX;
+    //     for(int i=x; i<y; i++){
+    //         ans = min(ans, maxi[{x,i}] * maxi[{i+1,y}] + solveMem(x, i, arr, maxi, dp) + solveMem(i+1, y, arr, maxi, dp));
+    //         dp[x][y] = ans;
+    //     }
+    //     return dp[x][y];
+    // }
+
+    int solveTab(vector<int>& arr,  map<pair<int,int>, int>& maxi){
         int n = arr.size();
-        if(x == y)
-            return 0;
-
-        if(dp[x][y] != -1)
-            return dp[x][y];
-
-        int ans = INT_MAX;
-        for(int i=x; i<y; i++){
-            ans = min(ans, maxi[{x,i}] * maxi[{i+1,y}] + solve(x, i, arr, maxi, dp) + solve(i+1, y, arr, maxi, dp));
-            dp[x][y] = ans;
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        
+        for(int x=n; x>=0; x--){
+            for(int y=1; y<n; y++){
+                int ans = INT_MAX;
+                if(x == y)
+                    continue;
+                else{
+                    for(int i=x; i<y; i++){
+                        ans = min(ans, maxi[{x,i}] * maxi[{i+1,y}] + dp[x][i] + dp[i+1][y]);
+                        dp[x][y] = ans;
+                    }
+                }
+            }
         }
-        return dp[x][y];
+        return dp[0][n-1];
     }
 
     int mctFromLeafValues(vector<int>& arr) {
         map<pair<int,int>, int> maxi;
         int n = arr.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        //vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
 
         for(int i=0; i<n; i++){
             maxi[{i,i}] = arr[i];
@@ -28,6 +48,8 @@ public:
                 maxi[{i,j}] = max(arr[j], maxi[{i,j-1}]);
             }
         }
-        return solve(0, n-1, arr, maxi, dp);
+        //return solveMem(0, n-1, arr, maxi, dp);
+
+        return solveTab(arr, maxi);
     }
 };
