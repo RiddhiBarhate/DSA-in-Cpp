b5e3c6b1d2ff73d691153ffcb057af975a670a98
Riddhi Barhate <riddhibarhate@gmail.com>
2024-05-01 20:14:15 +0530

    Time: 488 ms (15.98%), Space: 222 MB (23.33%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+
+    int solveMem(int index, int buy, int limit, vector<int>& prices, vector<vector<vector<int>>>& dp){
+        int n = prices.size();
+        if(index == n)
+            return 0;
+
+        if(limit == 0)
+            return 0;
+
+        if(dp[index][buy][limit] != -1)
+            return dp[index][buy][limit];
+
+        int profit = 0;
+        
+        if(buy){
+            int buykaro = -prices[index] + solveMem(index+1, 0, limit, prices, dp);
+            int skipkaro = 0 + solveMem(index+1, 1, limit, prices, dp);
+            profit = max(buykaro, skipkaro);
+        }
+        else{
+            int sellkaro = +prices[index] + solveMem(index+1, 1, limit-1, prices, dp);
+            int skipkaro = 0 + solveMem(index+1, 0, limit, prices, dp);
+            profit = max(sellkaro, skipkaro);
+        }
+
+        dp[index][buy][limit] = profit;
+        return dp[index][buy][limit];
+    }
+
+    // int solveTab(vector<int>& prices){
+    //     int n = prices.size();
+    //     vector<vector<int>> dp(n+1, vector<int> (2, 0));
+
+    //     int profit = 0;
+    //     for(int index=n-1; index>=0; index--){
+    //         for(int buy=0; buy<=1; buy++){
+    //             if(buy){
+    //                 int buykaro = -prices[index] + dp[index+1][0];
+    //                 int skipkaro = 0 + dp[index+1][1];
+    //                 profit = max(buykaro, skipkaro);
+    //             }
+    //             else{
+    //                 int sellkaro = +prices[index] + dp[index+1][1];
+    //                 int skipkaro = 0 + dp[index+1][0];
+    //                 profit = max(sellkaro, skipkaro);
+    //             }
+    //             dp[index][buy] = profit;
+    //         }
+    //     }
+ 
+    //     return dp[0][1];
+    // }
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>> (2, vector<int> (3, -1)));
+        return solveMem(0, 1, 2, prices, dp);
+
+        //return solveTab(prices);
+    }
+};
