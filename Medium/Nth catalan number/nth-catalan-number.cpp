f934af518baeca989f4438c8465b77bfd4474dc4
Riddhi Barhate <riddhibarhate@gmail.com>
2024-06-02 01:11:54 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Nth catalan number/nth-catalan-number.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    const int MOD = 1e9+7;
+    public:
+    //Function to find the nth catalan number.
+    int solve(int n)
+    {
+            
+        vector<long long> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            for(int j=0; j<i; j++){
+                dp[i] = (dp[i] + (dp[j] * dp[i-j-1]) % MOD) % MOD;
+            }
+        }
+        return dp[n];
+        
+    }
+    
+    int findCatalan(int n) 
+    {
+        //code here
+        return solve(n);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--) {
+	    
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    cout<< obj.findCatalan(n) <<"\n";    
+	}
+	return 0;
+}
+// } Driver Code Ends
