027311274430120958fc457069ac7f24227e29cf
Riddhi Barhate <riddhibarhate@gmail.com>
2024-06-05 00:42:45 +0530

    initial commit

---
+++ coding ninjas/Delete middle element from stack.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+
+void solve(stack<int>&inputStack, int count, int size){
+   if (count == size / 2) {
+     inputStack.pop();
+     return;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   solve(inputStack, count+1, size);
+   inputStack.push(num);
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   // Write your code here
+   solve(inputStack, 0, N);
+   
+}
