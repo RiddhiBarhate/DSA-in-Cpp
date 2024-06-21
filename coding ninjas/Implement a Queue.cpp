c6eb547f9e3b5ee77c50448ef42fef3f57bef25b
Riddhi Barhate <riddhibarhate@gmail.com>
2024-06-21 17:58:28 +0530

    initial commit

---
+++ coding ninjas/Implement a Queue.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h> 
+class Queue {
+    int *arr;
+    int size;
+    int qfront;
+    int rear;
+
+public:
+    Queue() {
+        // Implement the Constructor
+        size = 10001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty() {
+        if(qfront == rear){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void enqueue(int data) {
+        if(rear == size){
+            cout<<"Queue is full";
+        }
+        else{
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        if(qfront == rear){
+            return -1;
+        }
+        else{
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront == rear){
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }
+
+    int front() {
+        if(qfront == rear){
+            return -1;
+        }
+        else{
+            return arr[qfront];
+        }
+    }
+};
