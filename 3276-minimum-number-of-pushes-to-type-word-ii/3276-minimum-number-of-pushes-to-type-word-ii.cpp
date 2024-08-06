cb37398de80ab75c7418168d74ecd51ac12ff7e8
Riddhi Barhate <riddhibarhate@gmail.com>
2024-08-06 12:36:48 +0530

    Time: 69 ms (75.25%), Space: 25 MB (57.94%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        //count the frequency of each char
+        int arr[26];
+        for(char &item : word){
+            arr[item - 'a']++;
+        }
+
+        //sort array in decreasing order
+        sort(arr, arr + 26, greater<int>());
+
+        int count = 0;
+        int minPress = 0;
+
+        //assign no
+        for(int i = 0; i < 26; i++){
+            int value = arr[i] * ((count / 8) + 1);        //as we will assign values from 2 - 9
+            minPress += value;
+            count++;
+        }
+        return minPress;
+    }
+};
