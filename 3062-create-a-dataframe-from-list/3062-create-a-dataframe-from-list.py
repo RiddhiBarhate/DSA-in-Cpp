99144d912fd5be989564f5da2f8e5e65a52dc20c
Riddhi Barhate <riddhibarhate@gmail.com>
2024-08-11 18:06:45 +0530

    Initial commit

--- 3062-create-a-dataframe-from-list/3062-create-a-dataframe-from-list.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    df = pd.DataFrame(student_data, columns = ["student_id", "age"])
-
-    return df
-    
