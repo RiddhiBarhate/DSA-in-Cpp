836c646b5ea3934918e9b0bc3a1b32112ae85f91
Riddhi Barhate <riddhibarhate@gmail.com>
2024-08-14 00:35:00 +0530

    deleted

--- 3073-reshape-data-melt/3073-reshape-data-melt.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def meltTable(report: pd.DataFrame) -> pd.DataFrame:
-    unipivot = pd.melt(report, id_vars = 'product', var_name = 'quarter', value_name = 'sales')
-    return unipivot
-    
