55d05c21f310f1517d335bb9b21b1889ac2c783f
Riddhi Barhate <riddhibarhate@gmail.com>
2024-08-14 12:24:35 +0530

    Time: 7 ms (83.6%), Space: 13 MB (25.87%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        int n = nums.size();
+        //Approach 1
+        /*int n = nums.size();
         
         vector<int> diff;
         int i = 0;
@@ -16,6 +17,31 @@ public:
         }
 
         nth_element(diff.begin(), diff.begin() + (k-1), diff.end());    //sort only till kth value and return that value
-        return diff[k-1];
+        return diff[k-1];*/
+
+        //Approach 2
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0;
+        int right = nums[n-1] - nums[0];
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0, j = 0;
+            
+            // Use two pointers to count pairs with distance <= mid
+            for (int i = 0; i < n; i++) {
+                while (j < n && nums[j] - nums[i] <= mid) j++;
+                count += j - i - 1;
+            }
+            
+            if (count >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
     }
 };
