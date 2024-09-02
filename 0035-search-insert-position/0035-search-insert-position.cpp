dba2ebf2d07104a21793e9174c3d535aaff1e07d
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-02 19:24:49 +0530

    Time: 7 ms (22.69%), Space: 12.4 MB (6.52%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;   
+
+        //target needs to be inserted
+        if(target > nums[e])
+            return nums.size();    
+
+        //target is present in array
+        while(s <= e){
+
+            int mid = s + (e - s)/2;
+            if(target == nums[mid])
+                return mid;
+            else if(target < nums[mid])
+                e = mid - 1;
+            else
+                s = mid + 1;
+        }
+
+        return s;
+    }
+};
