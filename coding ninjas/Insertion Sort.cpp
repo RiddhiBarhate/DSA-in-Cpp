a3d93370b08a1ff5df76237bd1d4793dbd9fb0fe
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-05 07:48:10 +0530

    Sorting

---
+++ coding ninjas/Insertion Sort.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i=0; i<n; i++)
+    {
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0)
+        {
+            if(arr[j]>temp)
+            {
+                arr[j+1]=arr[j];
+            }
+            else
+            {
+                break;
+            }
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+}
