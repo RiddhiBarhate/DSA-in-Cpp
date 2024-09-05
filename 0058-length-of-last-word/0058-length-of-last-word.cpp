77959e87211b58bc9a0cf258558421413e7db079
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-05 08:46:59 +0530

    String

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -4,9 +4,9 @@ public:
         int n = s.length();
         int count = 0;
         for(int i = n-1; i >= 0; i--){
-            if(count == 0 && s[i] == ' ')
+            if(count == 0 && s[i] == ' ')   //end space
                 continue;
-            else if(count > 0 && s[i] == ' ')
+            else if(count > 0 && s[i] == ' ')   //space before last word
                 break;
             else
                 count++;
