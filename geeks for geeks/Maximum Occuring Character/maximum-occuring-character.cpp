d5a59e40ed0dc2ffeb09975bcf7eb058797ce113
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-11 00:45:52 +0530

    String

--- geeks for geeks/Maximum Occuring Character/maximum-occuring-character.cpp
+++ geeks for geeks/Maximum Occuring Character/maximum-occuring-character.cpp
@@ -12,7 +12,6 @@ class Solution
     //Function to find the maximum occurring character in a string.
     char getMaxOccuringChar(string str)
     {
-        // Your code here
         int arr[26]={0};
         for(int i=0;i<str.length();i++)
         {
