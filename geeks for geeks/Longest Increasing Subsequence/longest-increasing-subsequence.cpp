4e170cabe5fbac044cbd35c4a6f62ad45bfcc77b
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-11 00:45:21 +0530

    Array

--- geeks for geeks/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
+++ geeks for geeks/Longest Increasing Subsequence/longest-increasing-subsequence.cpp
@@ -8,13 +8,15 @@ using namespace std;
 class Solution{
     public:
     int longestSubsequence(int n, int a[]){
-       if(n==0) return 0;
+       if(n==0) 
+            return 0;
        
        vector<int>ans;
        ans.push_back(a[0]);
        
        for(int i=1; i<n; i++){
-           if(a[i] > ans.back()) ans.push_back(a[i]);
+           if(a[i] > ans.back()) 
+                ans.push_back(a[i]);
            else{
                // index of element just greater in ans
                int index = lower_bound(ans.begin(), ans.end(), a[i]) - ans.begin();
