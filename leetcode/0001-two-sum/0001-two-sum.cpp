e80bb416c74fa51b7efaaf3832ed3afb22145fba
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-13 19:01:31 +0530

    Array

--- leetcode/0001-two-sum/0001-two-sum.cpp
+++ leetcode/0001-two-sum/0001-two-sum.cpp
@@ -7,7 +7,7 @@ public:
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] + nums[j] == target) {
                     return {i, j};
-            }
+                }
             }
         }
         return {};
