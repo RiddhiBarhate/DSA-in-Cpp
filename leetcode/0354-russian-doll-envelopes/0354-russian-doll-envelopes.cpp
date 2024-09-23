80bd3c7a549187c046a528b726ce56d2986c0289
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-23 23:57:31 +0530

    Array

--- leetcode/0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ leetcode/0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,6 +1,6 @@
 class Solution {
     static bool cmp(vector<int>& a, vector<int>& b){
-        if(a[0]==b[0]) return a[1] > b[1];
+        if(a[0] == b[0]) return a[1] > b[1];
         return a[0] < b[0];
     }
 public:
