f8cd709bf4d607235093da0ad6422b724676f5c1
Riddhi Barhate <riddhibarhate@gmail.com>
2024-09-23 23:50:24 +0530

    Dynamic Programming

--- leetcode/1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
+++ leetcode/1952-minimum-sideway-jumps/1952-minimum-sideway-jumps.cpp
@@ -6,7 +6,7 @@ public:
         vector<int>dp1(n,n), dp2(n,n), dp3(n,n);
         dp1[0] = 1; dp2[0] = 0; dp3[0] = 1;
 
-        for(int i=1; i<n; i++){
+        for(int i = 1; i < n; i++){
             if(arr[i-1] != 1 && arr[i] != 1) dp1[i] = dp1[i-1];
             if(arr[i-1] != 2 && arr[i] != 2) dp2[i] = dp2[i-1];
             if(arr[i-1] != 3 && arr[i] != 3) dp3[i] = dp3[i-1];
