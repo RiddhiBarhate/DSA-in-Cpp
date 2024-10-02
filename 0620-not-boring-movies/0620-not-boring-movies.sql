16b0777cdcc9f23c50408c668eb0f26fe548221c
Riddhi Barhate <riddhibarhate@gmail.com>
2024-10-02 13:24:19 +0530

    Array

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT *
-FROM Cinema
-WHERE id % 2 != 0
-AND description != "boring"
-ORDER BY rating DESC
--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees
-LEFT JOIN EmployeeUNI 
-ON
-Employees.id = EmployeeUNI.id
