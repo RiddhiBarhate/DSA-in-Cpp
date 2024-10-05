8f88931b428e534b5d3a0860c78b53a57003cffc
Riddhi Barhate <riddhibarhate@gmail.com>
2024-10-05 11:39:50 +0530

    Time: 10 ms (41.85%), Space: 8.8 MB (50.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -7,7 +7,8 @@ public:
         if(n > m)
             return false;
 
-        sort(s1.begin(), s1.end());
+        //Approach 1 : Brute force
+        /*sort(s1.begin(), s1.end());
 
         for(int i = 0; i <= m-n; i++){
             string temp = s2.substr(i, n);
@@ -18,6 +19,30 @@ public:
                 return true;
         }
 
+        return false;*/
+
+        //Approach 2 : Sliding Window
+        vector<int> s1_freq(26, 0);
+        vector<int> s2_freq(26, 0);
+
+        for(char& ch : s1){
+            s1_freq[ch - 'a']++;
+        }
+
+        int i = 0, j = 0;
+        while(j < m){
+            s2_freq[s2[j] - 'a']++;
+
+            if((j-i+1) > n){
+                s2_freq[s2[i] - 'a']--;
+                i++;
+            }
+
+            if(s1_freq == s2_freq)
+                return true;
+
+            j++;
+        }
         return false;
     }
 };
