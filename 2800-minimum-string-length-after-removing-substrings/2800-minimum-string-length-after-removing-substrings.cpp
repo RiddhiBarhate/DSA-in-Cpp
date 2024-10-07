6a3e7d6b87c7651c5ee750dfb819e8308233d3ec
Riddhi Barhate <riddhibarhate@gmail.com>
2024-10-07 22:13:04 +0530

    Time: 0 ms (100%), Space: 10 MB (97.42%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minLength(string s) {
+        int n = s.length();
+        int i = 0, j = 1;
+
+        while(j < n){
+
+            if(i < 0){
+                i++;
+                s[i] = s[j];
+            }
+            else if((s[i] == 'A' && s[j] == 'B') || (s[i] == 'C' && s[j] == 'D')){
+                i--;
+            }
+            else{
+                i++;
+                s[i] = s[j];
+            }
+
+            j++;
+        }
+
+        return i + 1;
+    }
+};
