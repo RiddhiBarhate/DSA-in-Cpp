f103d3a2601794057193f217f372e016e2d0c88f
Riddhi Barhate <riddhibarhate@gmail.com>
2024-11-05 21:15:11 +0530

    Time: 0 ms (100%), Space: 8.2 MB (55.37%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isCircularSentence(string sentence) {
+        int n = sentence.length();
+        if(sentence[0] != sentence[n-1])
+            return false;
+
+        for(int i = 1; i < n; i++){
+            if(sentence[i] == ' '){
+                if(sentence[i-1] != sentence[i+1])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
