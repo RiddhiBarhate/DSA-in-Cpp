997ebf89d50511813dea6a67ed8e320fc709e7b0
Riddhi Barhate <riddhibarhate@gmail.com>
2024-11-09 17:06:44 +0530

    Time: 1746 ms (6.25%), Space: 8.3 MB (92.66%) - LeetHub

---
+++ 3394-minimum-array-end/3394-minimum-array-end.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long num = x;
+
+        for(int i = 1; i < n; i++) {
+
+            //num+1
+            num = (num+1) | x;
+            
+        }
+
+        return num;
+    }
+};
