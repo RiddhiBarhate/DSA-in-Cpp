97583d94542bddd558fd0b85bce6b14593aaefc0
Riddhi Barhate <riddhibarhate@gmail.com>
2024-11-25 20:14:49 +0530

    Time: 194 ms (42.33%), Space: 56.4 MB (17.88%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size();
+        int n = box[0].size();
+
+        vector<vector<char>> result(n, vector<char>(m));
+
+        //Transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                result[i][j] = box[j][i];
+            }
+        }
+
+        //90 degree k lie - Reverse kardo ahr row ko
+        for(vector<char>& row : result) {
+            reverse(begin(row), end(row));
+        }
+
+
+        //Apply the gravity
+          
+    
+        for(int j = 0; j < m; j++) {
+            int spaceBottomRow = n-1;
+            for(int i = n-1; i >= 0; i--) {
+                if(result[i][j] == '*') {
+                    spaceBottomRow = i-1;
+                    continue;
+                }
+
+                if(result[i][j] == '#') {
+                    result[i][j] = '.';
+                    result[spaceBottomRow][j] = '#';
+                    spaceBottomRow--;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
