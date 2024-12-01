748a6eeda678e228b7a251a3fc5b4c4d392aa46f
Riddhi Barhate <riddhibarhate@gmail.com>
2024-12-01 20:13:14 +0530

    Time: 0 ms (100%), Space: 13.3 MB (35.64%) - LeetHub

---
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        int n = arr.size();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(arr[i] == 2 * arr[j] && i != j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
