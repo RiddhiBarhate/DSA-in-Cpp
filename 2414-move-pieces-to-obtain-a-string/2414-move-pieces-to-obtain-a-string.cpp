557135fe03ca2fb98499da9676095c6d9298c6ba
Riddhi Barhate <riddhibarhate@gmail.com>
2024-12-05 12:15:58 +0530

    Time: 8 ms (71.26%), Space: 21.9 MB (33.33%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n = start.length();
+        int i = 0, j = 0;
+
+        while(i < n || j < n){
+            while(start[i] == '_'){
+                i++;
+            }
+
+            while(target[j] == '_'){
+                j++;
+            }
+
+            if(i == n || j == n)
+                return i == n && j == n;
+
+            if(start[i] != target[j])
+                return false;
+
+            if(start[i] == 'L' && i < j)
+                return false;
+
+            if(start[i] == 'R' && i > j)
+                return false;
+
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
