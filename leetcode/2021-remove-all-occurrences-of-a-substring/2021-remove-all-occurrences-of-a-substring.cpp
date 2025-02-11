6f244771ec3dbeabaf23a130b157bd32129d15bc
Riddhi Barhate <riddhibarhate@gmail.com>
2025-02-11 11:44:47 +0530

    String

--- leetcode/2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ leetcode/2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,28 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        while(s.length() != 0 && s.find(part) < s.length())
+
+        //Approach 1
+        /*while(s.length()!=0 && s.find(part)<s.length())
         {
             s.erase(s.find(part),part.length());
         }
-        return s;
+        return s;*/
+
+        //Approach 2
+        int partLength = part.length();
+        char partEnd = part.back();
+        string result;
+
+        for(char ch : s){
+            result.push_back(ch);
+
+            if(ch == partEnd && result.length() >= partLength){
+                if(result.substr(result.length() - partLength) == part){
+                    result.erase(result.length() - partLength);
+                }
+            }
+        }
+        return result;
     }
 };
