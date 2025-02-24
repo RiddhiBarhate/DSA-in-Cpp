b36335e4fdef923d97882babfbb81de8bc8a8484
Riddhi Barhate <riddhibarhate@gmail.com>
2025-02-24 22:07:18 +0530

    Time: 219 ms (39.83%), Space: 172.9 MB (32.34%) - LeetHub

---
+++ 2564-most-profitable-path-in-a-tree/2564-most-profitable-path-in-a-tree.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> adj;
+    unordered_map<int, int> bobMap;
+    int aliceIncome;
+    bool DFSBob(int curr, int t, vector<bool>&  visited) {
+        visited[curr] = true;
+        bobMap[curr] = t;
+
+        if(curr == 0) { //reached target
+            return true;
+        }
+
+        for(auto &ngbr : adj[curr]) {
+            if(!visited[ngbr]) {
+                if(DFSBob(ngbr, t+1, visited) == true) {
+                    return true;
+                }
+            }
+        }
+
+        bobMap.erase(curr);
+        return false;
+    }
+
+    void DFSAlice(int curr, int t, int income, vector<bool>& visited, vector<int>& amount) {
+        visited[curr] = true;
+
+        if(bobMap.find(curr) == bobMap.end() || t < bobMap[curr]) {
+            income += amount[curr];
+        } else if(t == bobMap[curr]) {
+            income += (amount[curr]/2);
+        }
+
+
+        if(adj[curr].size() == 1 && curr != 0) { //leaf node
+            aliceIncome = max(aliceIncome, income);
+        }
+
+        for(int &ngbr : adj[curr]) {
+            if(!visited[ngbr]) {
+                DFSAlice(ngbr, t+1, income, visited, amount);
+            }
+        }
+
+    }
+
+    int mostProfitablePath(vector<vector<int>>& edges, int bob, vector<int>& amount) {
+        int n = amount.size(); //n nodes (0 to n-1)
+
+        aliceIncome = INT_MIN;
+        for(vector<int>& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        //DFS on Bob for finding time to reach each node till 0
+        int time = 0;
+        vector<bool> visited(n, false);
+        DFSBob(bob, time, visited);
+        
+        int income = 0;
+        visited.assign(n, false);
+        DFSAlice(0, 0, income, visited, amount);
+
+        return aliceIncome;
+    }
+};
