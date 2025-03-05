aa14bc164e8a9251c116fc2eebe8156d1e2d6b9b
Riddhi Barhate <riddhibarhate@gmail.com>
2025-03-05 19:53:49 +0530

    Time: 3 ms (20.68%), Space: 7.8 MB (61.42%) - LeetHub

---
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+        long long cells = 1;
+
+        for(int i = 1; i <= n; i++){
+            cells += 4 * (i-1);
+        }
+
+        return cells;
+    }
+};
